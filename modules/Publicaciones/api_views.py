a788ca11fd51f9a6e546b1fcb8bbcb5e948fff04
orosas <jorosas@gmail.com>
2017-02-16 19:47:18 -0600

    CRUD Django REST API terminado en clase. Trabajo en archivo urls_api.py

--- modules/Publicaciones/api_views.py
+++ modules/Publicaciones/api_views.py
@@ -2,7 +2,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .serializers import UserSerializer,UserSecondSerializer
+#from .serializers import UserSerializer,UserSecondSerializer
+# PARA LA SEGUNDA clase
+from .serializers import *
+from django.shortcuts import get_object_or_404
 
 #Vistas basadas en clases
 
@@ -23,3 +26,33 @@ class UserList(APIView):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Métodos para completar el CRUD
+class UserDetail(APIView):
+
+    #
+    def get(self, request, pk):
+
+        user = get_object_or_404(User,pk=pk)
+
+        if user is not None:
+            serializer = UserFirstSerializer(user)
+            return Response(serializer.data, status = status.HTTP_200_OK)
+
+    # Para modificar un registro individual de usuario en la base
+    def put(self, request,pk):
+        user = get_object_or_404(User,pk=pk)
+
+        if user is not None:
+            serializer = UserFirstSerializer(instance=user,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = get_object_or_404(User,pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- modules/Publicaciones/urls_api.py
+++ modules/Publicaciones/urls_api.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .api_views import UserList
+from .api_views import UserList, UserDetail
 
 urlpatterns = [
     url(r'^$', UserList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', UserDetail.as_view()),
 ]

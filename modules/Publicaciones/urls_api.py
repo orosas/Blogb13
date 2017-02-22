a2ac7c1973afef444c39edfb79fa9cbc52e17fcc
orosas <jorosas@gmail.com>
2017-02-21 20:38:37 -0600

    Clase antes de deplo heroku

--- modules/Publicaciones/urls_api.py
+++ modules/Publicaciones/urls_api.py
@@ -6,6 +6,6 @@ from .api_views import UserList, UserDetail, PublicacionList, PublicacionDetail
 urlpatterns = [
     url(r'^$', UserList.as_view()),
     url(r'^(?P<pk>[0-9]+)/$', UserDetail.as_view()),
-    url(r'^publicacionesapi/$', PublicacionList.as_view()),
-    url(r'^publicacionesapi/(?P<pk>[0-9]+)/$', PublicacionDetail.as_view()),
+    url(r'^publicacionesapi/$', PublicacionList.as_view(), name='api-list-publicacion'),
+    url(r'^publicacionesapi/(?P<pk>[0-9]+)/$', PublicacionDetail.as_view(), name='api-detail-publicaciones'),
 ]
